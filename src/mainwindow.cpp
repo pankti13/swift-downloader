49f7ac0d80a9146fa6b8a6f1be9f3c4146865948
pankti13 <pankti221365@gmail.com>
2024-12-22 18:35:23 +0530

    Update UI of progress page

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,27 +5,48 @@
 #include <QDateTime>
 #include <QPushButton>
 #include <QHeaderView>
+#include <QProgressBar>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       stackedWidget(new QStackedWidget(this)),
       inputForm(new InputForm(this)),
-      progressPage(new QWidget(this)) {
+      progressPage(new QWidget(this)),
+      networkManager(new QNetworkAccessManager(this)) {
 
     // Setup progress page
-    QTableWidget *progressTable = new QTableWidget(progressPage);
-    progressTable->setColumnCount(4);
-    progressTable->setHorizontalHeaderLabels({"DateTime", "URL", "File Name", "Save Location"});
+    progressTable = new QTableWidget(progressPage);
+    progressTable->setColumnCount(5);
+    progressTable->setHorizontalHeaderLabels({"DateTime", "File Name", "Downloaded/Total", "Status", "Remaining Time"});
     progressTable->horizontalHeader()->setStretchLastSection(true);
+    progressTable->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 
+    progressBar = new QProgressBar(progressPage);
+    progressBar->setRange(0, 100);
+    progressBar->setValue(0);
+
+    QPushButton *pauseButton = new QPushButton("Pause", progressPage);
+    QPushButton *stopButton = new QPushButton("Stop", progressPage);
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
+    buttonLayout->addWidget(pauseButton);
+    buttonLayout->addWidget(stopButton);
+
+    QPushButton *backButton = new QPushButton("Back", progressPage);
     QVBoxLayout *progressLayout = new QVBoxLayout(progressPage);
     progressLayout->addWidget(progressTable);
-    QPushButton *backButton = new QPushButton("Back", progressPage);
+    progressLayout->addWidget(progressBar);
+    progressLayout->addLayout(buttonLayout);
     progressLayout->addWidget(backButton);
 
     connect(backButton, &QPushButton::clicked, [this]() {
         stackedWidget->setCurrentWidget(inputForm);
     });
+
+    connect(pauseButton, &QPushButton::clicked, this, &MainWindow::pauseDownload);
+    connect(stopButton, &QPushButton::clicked, this, &MainWindow::stopDownload);
 
     // Setup stacked widget
     stackedWidget->addWidget(inputForm);
@@ -50,13 +71,27 @@ void MainWindow::handleFormSubmission() {
     progressTable->insertRow(row);
 
     progressTable->setItem(row, 0, new QTableWidgetItem(QDateTime::currentDateTime().toString()));
-    progressTable->setItem(row, 1, new QTableWidgetItem(url));
-    progressTable->setItem(row, 2, new QTableWidgetItem(fileName));
-    progressTable->setItem(row, 3, new QTableWidgetItem(saveLocation));
+    progressTable->setItem(row, 1, new QTableWidgetItem(fileName));
+    progressTable->setItem(row, 2, new QTableWidgetItem("0/0"));
+    progressTable->setItem(row, 3, new QTableWidgetItem("To Be Started"));
+    progressTable->setItem(row, 4, new QTableWidgetItem("N/A"));
 
     stackedWidget->setCurrentWidget(progressPage);
 }
 
 void MainWindow::handleFormCancellation() {
     close();
+}
+
+void MainWindow::updateStatus(const QString &status) {
+    int row = progressTable->rowCount() - 1; 
+    progressTable->setItem(row, 3, new QTableWidgetItem(status));
+}
+
+void MainWindow::pauseDownload() {
+    updateStatus("Paused");
+}
+
+void MainWindow::stopDownload() {
+    updateStatus("Stopped");
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -3,25 +3,40 @@
 
 #include <QMainWindow>
 #include <QStackedWidget>
+#include <QTableWidget>
+#include <QProgressBar>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QFile>
+#include <QPushButton>
+#include <QHBoxLayout>
 
 class InputForm;
 class QWidget;
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
     void handleFormSubmission();
     void handleFormCancellation();
 
+    void updateStatus(const QString &status);
+    void pauseDownload();
+    void stopDownload();
+
 private:
     QStackedWidget *stackedWidget;
     InputForm *inputForm;
     QWidget *progressPage;
+    QTableWidget *progressTable;
+    QProgressBar *progressBar;
+    QNetworkAccessManager *networkManager;
 };
 
 #endif
